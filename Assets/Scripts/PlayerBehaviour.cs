0e3aec8ed43897eecefd4c8b319287b98d219feb
lianmang-cs <lianmang238@gmail.com>
2026-01-29 15:08:37 -0600

    Instantiation of new objects.
    Instantiated the fruits, which allows the player to press the spacebar and drop the object each time its pressed.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,31 +5,35 @@ public class PlayerBehaviour : MonoBehaviour
 {
     //variable for speed of the player
     public float speed = 0.01f; 
-    public GameObject fruit; 
     public float offY = -0.6f; 
+    public GameObject fruit; 
+    private GameObject currentFruit; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update() {
         //Fruit position below player
-        if (fruit != null) {
+        if (currentFruit != null) {
             //current player position
             Vector3 playerPos = transform.position;
             //(x, y, z)
             Vector3 fruitOffset = new Vector3(0.0f, offY, 0.0f); 
-            fruit.transform.position = playerPos + fruitOffset;
+            currentFruit.transform.position = playerPos + fruitOffset;
+        }
+        else {
+            currentFruit = Instantiate(fruit, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
         }
         //Drop the fruit
         if (Keyboard.current.spaceKey.wasPressedThisFrame) {
-            Rigidbody2D body = fruit.GetComponent<Rigidbody2D>(); 
+            Rigidbody2D body = currentFruit.GetComponent<Rigidbody2D>(); 
             body.gravityScale = 1.0f; 
-            Collider2D collider = fruit.GetComponent<Collider2D>(); 
+            Collider2D collider = currentFruit.GetComponent<Collider2D>(); 
             collider.enabled = true; 
-            fruit = null; 
+            currentFruit = null; 
         }
         //Left move
         if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
