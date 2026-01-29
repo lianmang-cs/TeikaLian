ecc2db9aef4bf8cca72e420b2ce51d380cf1e45a
lianmang-cs <lianmang238@gmail.com>
2026-01-29 14:52:01 -0600

    Dropping the fruit with spacebar.
    Created two if statements. One if statement enabled the fruit to be adjusted with the player's position.
    The second if statement enabled the fruit to be drop when pressing the spacebar key. Also, the gravity is adjusted such that the force of gravity on the fruit that is waiting to be dropped is not constantly being increased.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
     //variable for speed of the player
-    public float speed; 
+    public float speed = 0.01f; 
+    public GameObject fruit; 
+    public float offY = -0.6f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,19 +15,33 @@ public class PlayerBehaviour : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        //Left arrow key
+        //Fruit position below player
+        if (fruit != null) {
+            //current player position
+            Vector3 playerPos = transform.position;
+            //(x, y, z)
+            Vector3 fruitOffset = new Vector3(0.0f, offY, 0.0f); 
+            fruit.transform.position = playerPos + fruitOffset;
+        }
+        //Drop the fruit
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+            Rigidbody2D body = fruit.GetComponent<Rigidbody2D>(); 
+            body.gravityScale = 1.0f; 
+            Collider2D collider = fruit.GetComponent<Collider2D>(); 
+            collider.enabled = true; 
+            fruit = null; 
+        }
+        //Left move
         if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) {
             Vector3 newPos = transform.position;
             newPos.x = newPos.x - speed; 
             transform.position = newPos; 
         }
-        //Right arrow key
+        //Right move
         if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) {
             Vector3 newPos = transform.position;
             newPos.x = newPos.x + speed; 
             transform.position = newPos; 
-        }
-        
-        
+        }      
     }
 }
